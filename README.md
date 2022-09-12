abbfc2081eae05cf9af2097e542f4f5fa13c6b0b
uncu <uncu991@gmail.com>
2022-09-12 22:47:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Restaurant app with tailwind
+
+- using react
+- Tailwindcss
+### if you want use this project
+
+- git clone this repository
+- npm install
+- npm start
+
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Category from "./components/Category";
+import Food from "./components/Food";
+import HeaderCards from "./components/HeaderCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+   <>
+   < Navbar />
+   <Hero />
+   < HeaderCards />
+   <Food/>
+   < Category />
+   
+   </>
+  );
+}
+
+export default App;
